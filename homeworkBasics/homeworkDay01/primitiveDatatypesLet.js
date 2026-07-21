1727072de0f43fdc2c03ba3f7784d68857597a6c
Lalithagajend <40142505+Lalithagajend@users.noreply.github.com>
2026-07-21 16:52:23 +0530

    day01 and 02 assignments

---
+++ homeworkBasics/homeworkDay01/primitiveDatatypesLet.js
@@ -0,0 +1,22 @@
+//Create the following variables using let (not using var) and check their typeOf
+//a) firstName
+// b) companyName
+// c) mobileNumber
+// d) isAutomation
+// e) hasPlaywright (do not assign)
+//Print and confirm the values and data types
+// 1. String
+let firstName = `Lalitha`
+//1b. Strng
+let companyName = "ABT Parcel"
+//2. number
+let mobileNumber = 9898989898
+//3. Boolean
+let isAutomation = true
+//4. undefined
+let hasPlaywright
+console.log(firstName, ` is a `, typeof firstName)
+console.log(companyName, ` is a `,typeof companyName)
+console.log(mobileNumber, ` is a `,typeof mobileNumber)
+console.log(isAutomation, ` is a `,typeof isAutomation)
+console.log(hasPlaywright, ` is a `,typeof hasPlaywright)
---
+++ homeworkBasics/homeworkDay02/classroomPractice01.js
@@ -0,0 +1,8 @@
+//Write a JavaScript code to print odd numbers from 1 to 25.
+var number
+for (number=0;number<=25;number++){
+    if(number%2!==0){
+        console.log(number)
+    }
+
+}
