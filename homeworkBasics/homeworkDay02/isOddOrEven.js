dc616fa9568092c21fe8f78c44bc6107b85410b2
Lalithagajend <40142505+Lalithagajend@users.noreply.github.com>
2026-07-21 18:54:54 +0530

    isOddOrEven

---
+++ homeworkBasics/homeworkDay02/isOddOrEven.js
@@ -0,0 +1,22 @@
+//1. Create a function named `isOddOrEven` that takes a number as a parameter
+//2. Declare and initialize the variable
+//3. Use a conditional statement to check if the number is divisible by 2
+//4. Call the function and print the result
+let number
+for(number=0; number<=9; number++){
+isOddOrEven(number)
+}
+//let number = check
+
+//console.log(number)
+function isOddOrEven(){
+    if  (number == 0 )
+    console.log("Zero entered. Zero is neither even nor odd")
+else if (number%2==0)
+console.log(number,": is an even number")
+else if (number%2!=0 && number/1==number)
+console.log(number," : is an odd number")
+else
+console.log("Enter a valid number")
+}
+
