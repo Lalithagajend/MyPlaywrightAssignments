e81e0bad0c59c68d8b1f283890aad72d18fe03d9
Lalithagajend <40142505+Lalithagajend@users.noreply.github.com>
2026-07-21 19:14:54 +0530

    new hw

---
+++ homeworkBasics/homeworkDay02/checkNumPositiveNegativeZero.js
@@ -0,0 +1,12 @@
+let number = 4
+function checkNumber(){
+    if (number==0)
+    console.log(number, ` is zero`)
+    else if (number<0)
+    console.log(number,` is negative`)
+    else if (number>0)
+    console.log(number, ` is positive`)
+    else
+    console.log(`Enter a valid number`)
+}
+checkNumber()
